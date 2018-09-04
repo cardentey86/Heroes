2b2a09df412c136eb7f864bb88dfe8d82ad183a9
cardentey86 <35451033+cardentey86@users.noreply.github.com>
2018-09-04 16:14:55 -0400

    update

--- FrontEnd/AngularBaseMaterial/src/app/hero-detail/hero-detail.component.ts
+++ FrontEnd/AngularBaseMaterial/src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../heroes/heroe';
+import {ActivatedRoute} from '@angular/router';
+import {HeroService} from '../hero.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-hero-detail',
@@ -10,9 +13,25 @@ export class HeroDetailComponent implements OnInit {
 
   @Input() hero: Hero;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private heroService: HeroService,
+              private location: Location) { }
 
-  ngOnInit() {
-  }
+    ngOnInit(): void {
+        this.getHero();
+    }
+
+    // Obtiene el heroe seleccionado
+    getHero(): void {
+        const id = + this.route.snapshot.paramMap.get('id');
+        this.heroService.getHero(id)
+            .subscribe(hero => this.hero = hero);
+    }
+
+
+    // Retorna a la vista de donde llegó
+    goBack(): void {
+        this.location.back();
+    }
 
 }
--- FrontEnd/AngularBaseMaterial/src/app/heroes/heroes.component.ts
+++ FrontEnd/AngularBaseMaterial/src/app/heroes/heroes.component.ts
@@ -11,19 +11,13 @@ import {map} from 'rxjs/internal/operators';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes: Hero;
-
-  selectedHero: Hero;
+  heroes: Hero[];
 
   constructor(private heroService : HeroService) { }
 
   ngOnInit() {
       this.getHeroes();
   }
-
-    onSelect(hero: Hero): void {
-        this.selectedHero = hero;
-    }
 
     getHeroes(): void {
         this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
